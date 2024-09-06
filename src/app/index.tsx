4b1adaf70d1c081e39b01a5ef1c2df657571b4d5
ashik <azashikdev@gmail.com>
2024-09-06 23:58:08 +0600

    first commit

---
+++ src/app/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+import React from "react";
+import Gree from "../components/Gree";
+
+const index = () => {
+  return (
+    <View>
+      <Gree />
+    </View>
+  );
+};
+
+export default index;
